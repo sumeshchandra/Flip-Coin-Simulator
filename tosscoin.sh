ccd0920e5d39c3b7e9f64bd26377df2a35d9e523
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-06-21 00:11:37 +0530

    Target won 21 times

--- tosscoin.sh
+++ tosscoin.sh
@@ -1,20 +1,37 @@
 #! /bin/bash -x
 
 echo "Flip coin simulator"
-echo "number of times user want to flip the coin"
-read flip
-echo $flip
-countHead=0
-countTail=0
-declare -A noOfCounts
-for((i=0;i<flip;i++))
+target_count=21
+minimum_difference=0
+heads_count=0
+tails_count=0
+flips_count=0
+while [ 1 ]
 do
-if [ $((RANDOM%2)) -eq 1 ]
+toss=$(( RANDOM % 2  ))
+if(( toss == 0 ))
 then
-((countHead++))
+echo "Heads"
+(( heads_count++ ))
+
 else
-((countTail++))
+echo "Tails"
+(( tails_count++ ))
+
 fi
+
+
+diff=$(( heads_count - tails_count ))
+if [[ $heads_count -eq $target_count  ||  $tails_count -eq $target_count ]] && [ $diff -eq 0 ];then
+echo "Tie "
+break
+
+elif [ $heads_count -eq $target_count ] && [ $diff -gt $minimum_difference ];then
+        echo "Heads won by $diff"
+        break
+elif [ $tails_count -eq $target_count ] && [ $diff -gt $minimum_difference ];then
+         echo "Tails won by ${diff} points"
+         break
+fi
+
 done
-echo "head won="$countHead
-echo "tail won="$countTail
