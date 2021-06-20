e81a6aabe79de7fff6e8b82a56f4dfd37373b107
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-06-21 00:07:14 +0530

    calaulate no times won

--- tosscoin.sh
+++ tosscoin.sh
@@ -1,11 +1,20 @@
-#! /bin/bash
+#! /bin/bash -x
 
 echo "Flip coin simulator"
-flipCoin=$((RANDOM%2))
-
-if [ $flipCoin -eq 1 ]
+echo "number of times user want to flip the coin"
+read flip
+echo $flip
+countHead=0
+countTail=0
+declare -A noOfCounts
+for((i=0;i<flip;i++))
+do
+if [ $((RANDOM%2)) -eq 1 ]
 then
-    echo "TAIL"
+((countHead++))
 else
-    echo "HEAD"
+((countTail++))
 fi
+done
+echo "head won="$countHead
+echo "tail won="$countTail
